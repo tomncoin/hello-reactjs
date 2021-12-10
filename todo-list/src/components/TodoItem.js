0bea2c95e48bd154dfcb1ff4d8ec52eb07b86a17
blue <blue@DESKTOP-Q1MIF3C>
2021-12-10 15:49:01 +0700

    check iamge for todoitem

--- todo-list/src/components/TodoItem.js
+++ todo-list/src/components/TodoItem.js
@@ -6,13 +6,18 @@ class TodoItem extends Component{
     render(){
         
         const {item, onClick}=this.props;
+        let checkImg ="https://cdn-icons-png.flaticon.com/128/87/87932.png";
+        if(item.isCompleted){
+            checkImg ="https://cdn-icons.flaticon.com/png/128/2251/premium/2251677.png?token=exp=1639125883~hmac=568c970f958be48674380a2884c01a64";
+        }
 
         let className="TodoItem";
         if(item.isCompleted){
             className += " TodoItemDone";
         }
         return(
-            <div className={className} onClick={onClick}>
+            <div className={className}>
+                <img  onClick={onClick} src={checkImg} width="32" height="32" />
                 <p>{this.props.item.title}</p>
             </div>
         );
