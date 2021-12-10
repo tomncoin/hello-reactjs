be23d75d404896cf5ade8bf434008421477f02f6
blue <blue@DESKTOP-Q1MIF3C>
2021-12-10 14:21:31 +0700

    use stage in App.js: for traffic-light

--- traffic-light/src/App.js
+++ traffic-light/src/App.js
@@ -3,21 +3,88 @@ import './App.css';
 import TrafficLight from './components/TrafficLight';
 import { Component } from 'react';
 
+const RED=0;
+const ORANGE=1;
+const GREEN=2;
+
 class App extends Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
+
+    this.state={
+      initial: 'state',
+      currentColor: GREEN  
+    };
+
+    // setInterval(()=>{
+    //     this.setState({currentColor: this.getNextColor(this.state.currentColor)}); 
+    // },this.trafficDelay(this.state.currentColor));
+
+    this.clickMe = this.clickMe.bind(this);
+    this.clickRed = this.clickRed.bind(this);
+  }
+
+  
+  getNextColor(color){
+    switch(color){
+        case RED:
+            return ORANGE;
+        case ORANGE:
+            return GREEN;
+        default:
+            return RED;
+
+    };
+  }
+
+  trafficDelay = (color) => {
+    switch (color) {
+       case 0: return 5000;
+       case 1: return 500;
+       default: return 10000;
+    }
+ }
+
+  clickMe () {
+    this.setState({
+      currentColor: this.getNextColor(this.state.currentColor)
+    });
+   }
+
+  clickRed () {
+    this.setState({
+      currentColor: RED
+    })
+   }
+
+  componentDidMount() {
+    this.timerID = setInterval(()=>{
+      console.log(this.state.currentColor+" "+ this.trafficDelay(this.state.currentColor));
+      this.setState({currentColor: this.getNextColor(this.state.currentColor)}); 
+    },this.trafficDelay(this.getNextColor(this.state.currentColor)));
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
   }
 
   render(){
-    // const {currentColor}=this.state;
+    const {currentColor}=this.state;
     return (
       <div className="App">
         {
-          <TrafficLight></TrafficLight>
+          <TrafficLight currentColor={currentColor}></TrafficLight>
         }
+        <div>
+          <button onClick={this.clickMe}>click</button>
+          <button onClick={this.clickRed}>red</button>
+        </div>
       </div>
     );
   }
+
+  
 }
+
 
 export default App;
--- traffic-light/src/components/TrafficLight.js
+++ traffic-light/src/components/TrafficLight.js
@@ -7,38 +7,16 @@ const ORANGE=1;
 const GREEN=2;
 
 class TrafficLight extends Component{
-    constructor(){
-        super();
-        this.state={
-          currentColor: GREEN  
-        };
-
-        setInterval(()=>{
-            this.setState({currentColor: this.getNextColor(this.state.currentColor)});
-            //console.log(this.currentColor);
-        },2000);
-    }
-
-    getNextColor(color){
-        switch(color){
-            case RED:
-                return ORANGE;
-            case ORANGE:
-                return GREEN;
-            default:
-                return RED;
     
-        };
-    }
     
     render(){
-        // console.log('rendering...');
+        const {currentColor}=this.props;
 
         return(
             <div className="TrafficLight">
-                <div className={classNames('buld','red',{active: this.state.currentColor===RED})}></div>
-                <div className={classNames('buld','orange',{active: this.state.currentColor===ORANGE})}></div>
-                <div className={classNames('buld','green',{active: this.state.currentColor===GREEN})}></div>
+                <div className={classNames('buld','red',{active: currentColor===RED})}></div>
+                <div className={classNames('buld','orange',{active: currentColor===ORANGE})}></div>
+                <div className={classNames('buld','green',{active: currentColor===GREEN})}></div>
             </div>
         );
     }
