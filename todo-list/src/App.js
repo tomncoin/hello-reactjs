e04d84d5204b20c10c900a22db2e2002ca753502
blue <blue@DESKTOP-Q1MIF3C>
2021-12-10 15:12:43 +0700

    3-Immutability

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -6,20 +6,30 @@ import { Component } from 'react';
 class App extends Component { 
   constructor(){
     super();
-    this.todoItems=[
-      {title: 'Mua bim bim', isCompleted: true},
-      {title: 'Di da bong', isCompleted: false}, 
-      {title: 'Di cho', isCompleted: true}
-    ]
+    this.state={
+      todoItems: [
+        {title: 'Mua bim bim', isCompleted: true},
+        {title: 'Di da bong', isCompleted: false}, 
+        {title: 'Di cho', isCompleted: true}
+      ]
+    };
+
+    this.onItemClick = this.onItemClick.bind(this.state.todoItems);
   }
+
+  onItemClick(){
+    // this.props.item.isCompleted = !this.props.item.isCompleted;
+    console.log(this.index);
+  }
+
   render(){
     return (
       <div className="App">
         {
-          this.todoItems.length>0 && this.todoItems.map((item, index)=><TodoItem key={index} item={item}></TodoItem>)
+          this.state.todoItems.length>0 && this.state.todoItems.map((item, index)=><TodoItem key={index} item={item} onClick={this.onItemClick}></TodoItem>)
         }
         {
-          this.todoItems.length==0 && 'Nonthing here'
+          this.state.todoItems.length==0 && 'Nonthing here'
         }
       </div>
     );
--- todo-list/src/components/TodoItem.js
+++ todo-list/src/components/TodoItem.js
@@ -2,16 +2,17 @@ import React, {Component} from 'react';
 import './TodoItem.css';
 
 class TodoItem extends Component{
+   
     render(){
         
-        const {item}=this.props;
+        const {item, onClick}=this.props;
 
         let className="TodoItem";
         if(item.isCompleted){
             className += " TodoItemDone";
         }
         return(
-            <div className={className}>
+            <div className={className} onClick={onClick}>
                 <p>{this.props.item.title}</p>
             </div>
         );
