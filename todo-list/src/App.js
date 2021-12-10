c59ebcb43e5d2d5b06e27cddc4d6dfb9a9e3f5fc
blue <blue@DESKTOP-Q1MIF3C>
2021-12-10 10:10:13 +0700

    crate Component

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoItem from './components/TodoItem';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <TodoItem/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ todo-list/src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class TodoItem extends Component{
+    render(){
+        return(
+            <div className="TodoItem">
+                Đi chợ
+            </div>
+        );
+    };
+}
+
+export default TodoItem;
